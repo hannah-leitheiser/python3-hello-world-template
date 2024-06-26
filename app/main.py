5599cc16114239c2aa78b9942a758d979621f042
hannah-leitheiser <leitheiserhannah@runbox.com>
2024-06-25 23:09:30 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,13 @@
-#hello
+import argparse
 def main():
+    parser = argparse.ArgumentParser(description='Description of your script')
+    parser.add_argument('--optional_arg', type=int, default=0, help='Description of an optional argument')
+
+    args = parser.parse_args()
+
+    # Access the arguments
+    print(f'Optional Argument: {args.optional_arg}')
+    
     print("Hello, World!")
 
 if __name__ == "__main__":
